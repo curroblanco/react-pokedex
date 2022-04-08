153fd28aa22f23c76208f68da6ef1fcc9fd6d003
esra <esra.sayin@perseivivareium.com>
2022-04-08 22:13:39 +0200

    feature: added navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,17 +1,13 @@
 import React from "react";
-import { Nav, NavLink } from "./navbarElements";
-import { ReactComponent as Logo } from './logo.svg';
-import './logo.css';
+import { Nav } from "./navbarElements";
+import { ReactComponent as Logo } from "./logo.svg";
+import "./logo.css";
 
 const Navbar = () => {
   return (
-    <>
-      <Nav>
-        <NavLink to="/">
-        <Logo class="dark-shadow"/>
-        </NavLink>
-      </Nav>
-    </>
+    <Nav>
+      <Logo class="dark-shadow" />
+    </Nav>
   );
 };
 
--- src/components/navbar/navbarElements.js
+++ src/components/navbar/navbarElements.js
@@ -1,28 +1,15 @@
 import styled from 'styled-components';
-import { NavLink as Link } from 'react-router-dom';
+
   
 export const Nav = styled.nav`
   background: #EF5350;
-  height: 85px;
+  height: 6em;
   display: flex;
   justify-content: space-between;
-  padding: 0.2rem calc((100vw - 1000px) / 2);
+  padding: 0.2rem calc((100vw - 59em) / 2);
   z-index: 12;
-  /* Third Nav */
-  /* justify-content: flex-start; */
 `;
   
-export const NavLink = styled(Link)`
-  color: #808080;
-  display: flex;
-  align-items: center;
-  text-decoration: none;
-  padding: 0 1rem;
-  height: 100%;
-  cursor: pointer;
-  &.active {
-    color: #000000;
-  }
-`;
+
   
 
